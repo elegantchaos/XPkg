96ce87df1bb0bf37271045b119d7a605edfef109
Sam Deane <sam@elegantchaos.com>
2018-06-08 14:12:31 +0100

    Added basic runner.

---
+++ Sources/XPkg/Runner.swift
@@ -0,0 +1,60 @@
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// Created by Sam Deane, 08/06/2018.
+// All code (c) 2018 - present day, Elegant Chaos Limited.
+// For licensing terms, see http://elegantchaos.com/license/liberal/.
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+import Foundation
+
+class Runner {
+    let environment: [String:String]? = nil
+
+    struct Result {
+        let status: Int32
+        let stdout: String
+        let stderr: String
+    }
+
+    /**
+     Invoke a command and some optional arguments.
+     Control is transferred to the launched process, and this function doesn't return.
+     */
+
+    func exec(_ command : String, arguments: [String] = []) {
+        let process = Process()
+        process.launchPath = command
+        process.arguments = arguments
+        process.environment = self.environment
+        process.launch()
+        process.waitUntilExit()
+        exit(process.terminationStatus)
+    }
+
+
+    /**
+     Invoke a command and some optional arguments synchronously.
+     Waits for the process to exit and returns the captured output plus the exit status.
+     */
+
+    func sync(_ command : String, arguments: [String] = []) throws -> Result {
+        let pipe = Pipe()
+        let handle = pipe.fileHandleForReading
+        let errPipe = Pipe()
+        let errHandle = errPipe.fileHandleForReading
+
+        let process = Process()
+        process.launchPath = command
+        process.arguments = arguments
+        process.standardOutput = pipe
+        process.standardError = errPipe
+        process.environment = self.environment
+        process.launch()
+        let data = handle.readDataToEndOfFile()
+        let errData = errHandle.readDataToEndOfFile()
+        process.waitUntilExit()
+        let stdout = String(data:data, encoding:String.Encoding.utf8) ?? ""
+        let stderr = String(data:errData, encoding:String.Encoding.utf8) ?? ""
+        return Result(status: process.terminationStatus, stdout: stdout, stderr: stderr)
+    }
+
+}
