0339ef4a434b0ed5e56e40644117b5c9b6f43e9e
Sam Deane <sam@elegantchaos.com>
2018-06-14 11:50:29 +0100

    Hooked up builder.

--- Package.swift
+++ Package.swift
@@ -9,6 +9,8 @@ let package = Package(
       .executable(name: "xpkg", targets: ["XPkgCommand"]),
     ],
     dependencies: [
+        .package(url: "https://github.com/elegantchaos/Builder.git", from: "1.0.5"),
+        .package(url: "https://github.com/elegantchaos/BuilderConfiguration.git", from: "1.1.3"),
         .package(url: "https://github.com/elegantchaos/Logger", from: "1.0.8"),
         .package(url: "https://github.com/elegantchaos/Arguments", from: "1.0.1"),
         ],
@@ -19,6 +21,9 @@ let package = Package(
         .target(
             name: "XPkg",
             dependencies: ["Arguments", "Logger"]),
+        .target(
+            name: "Configure",
+          dependencies: ["BuilderConfiguration"]),
         .testTarget(
             name: "XPkgTests",
             dependencies: ["XPkg"]
---
+++ Sources/Configure/main.swift
@@ -0,0 +1,49 @@
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// Created by Sam Deane, 28/02/2018.
+// All code (c) 2018 - present day, Elegant Chaos Limited.
+// For licensing terms, see http://elegantchaos.com/license/liberal/.
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+import BuilderConfiguration
+
+let settings = Settings(specs: [
+    .base(
+        values: [
+        ],
+        inherits: [
+            .spec(name: "mac", filter: ["macOS"]),
+            .spec(name: "debug", filter: ["debug"])
+        ]
+    ),
+    .spec(
+        name: "mac",
+        values: [
+          .setting("minimum-target", "macosx10.13"),
+        ]
+    ),
+    .spec(
+        name: "debug",
+        values: [
+          .setting("optimisation", "none")
+        ]
+    )
+    ]
+)
+
+let configuration = Configuration(
+    settings: settings,
+    actions: [
+        .action(name:"build", phases:[
+            .buildPhase(name:"Building", target:"xpkg"),
+            ]),
+        .action(name:"test", phases:[
+            .testPhase(name:"Testing", target:"XPkgCommand"),
+            ]),
+        .action(name:"run", phases:[
+            .actionPhase(name:"Building", action: "build"),
+            .toolPhase(name:"Running", tool: "run", arguments:[]),
+            ]),
+    ]
+)
+
+configuration.outputToBuilder()
