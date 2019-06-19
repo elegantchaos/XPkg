f76bc34cfd48abe134b885232572767647306841
Sam Deane <sam@elegantchaos.com>
2019-06-20 00:31:28 +0100

    Suppressed logger startup message.

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/elegantchaos/Builder.git", from: "1.0.12"),
         .package(url: "https://github.com/elegantchaos/BuilderConfiguration.git", from: "1.1.3"),
-        .package(url: "https://github.com/elegantchaos/Logger", from: "1.3.6"),
+        .package(url: "https://github.com/elegantchaos/Logger", from: "1.3.7"),
         .package(url: "https://github.com/elegantchaos/Arguments", from: "1.0.4"),
         .package(url: "https://github.com/elegantchaos/Runner", from: "1.0.1"),
         ],
